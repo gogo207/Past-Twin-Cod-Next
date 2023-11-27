cadc5cd569cd8ae459bd39e0e777ea10a8e3e778
MO Nabil <gogo207@gmail.com>
2023-11-27 12:17:09 +0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,192 @@
+import React from 'react'
+import Head from 'next/head'
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Past Twin Cod</title>
+          <meta property="og:title" content="Past Twin Cod" />
+        </Head>
+        <Header></Header>
+        <div className="home-hero">
+          <div className="home-container1">
+            <h1 className="home-text">Magnificent things are very simple</h1>
+            <span className="home-text1">
+              <span>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  non volutpat turpis.
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span>
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+              <span>
+                <span>
+                  Mauris luctus rutrum mi ut rhoncus. Integer in dignissim
+                  tortor.
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span>
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+            </span>
+            <div className="home-btn-group">
+              <button className="home-button button">Get Started</button>
+              <button className="home-button1 button">Learn More</button>
+            </div>
+          </div>
+          <img
+            alt="image"
+            src="https://images.unsplash.com/photo-1525498128493-380d1990a112?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDI0fHxtaW5pbWFsaXNtJTIwZ3JlZW58ZW58MHx8fHwxNjI1ODQxMDcw&amp;ixlib=rb-1.2.1&amp;h=1200"
+            className="home-image"
+          />
+        </div>
+        <Footer></Footer>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .home-hero {
+            width: 100%;
+            display: flex;
+            padding: var(--dl-space-space-threeunits);
+            max-width: var(--dl-size-size-maxwidth);
+            min-height: 80vh;
+            align-items: center;
+            flex-direction: row;
+            justify-content: space-between;
+          }
+          .home-container1 {
+            display: flex;
+            margin-right: var(--dl-space-space-threeunits);
+            padding-right: var(--dl-space-space-threeunits);
+            flex-direction: column;
+          }
+          .home-text {
+            font-size: 3rem;
+            max-width: 450px;
+          }
+          .home-text1 {
+            margin-top: var(--dl-space-space-twounits);
+            margin-bottom: var(--dl-space-space-twounits);
+          }
+          .home-btn-group {
+            display: flex;
+            align-items: center;
+            flex-direction: row;
+          }
+          .home-button {
+            transition: 0.3s;
+            padding-top: var(--dl-space-space-unit);
+            padding-left: var(--dl-space-space-twounits);
+            padding-right: var(--dl-space-space-twounits);
+            padding-bottom: var(--dl-space-space-unit);
+          }
+          .home-button:hover {
+            color: var(--dl-color-gray-white);
+            background-color: var(--dl-color-gray-black);
+          }
+          .home-button1 {
+            transition: 0.3s;
+            margin-left: var(--dl-space-space-unit);
+            padding-top: var(--dl-space-space-unit);
+            border-color: transparent;
+            padding-left: var(--dl-space-space-twounits);
+            padding-right: var(--dl-space-space-twounits);
+            padding-bottom: var(--dl-space-space-unit);
+          }
+          .home-button1:hover {
+            border-color: var(--dl-color-gray-black);
+          }
+          .home-image {
+            width: 400px;
+            object-fit: cover;
+          }
+          @media (max-width: 991px) {
+            .home-hero {
+              flex-direction: column;
+            }
+            .home-container1 {
+              align-items: center;
+              margin-right: 0px;
+              margin-bottom: var(--dl-space-space-twounits);
+              padding-right: 0px;
+            }
+            .home-text {
+              text-align: center;
+            }
+            .home-text1 {
+              text-align: center;
+              padding-left: var(--dl-space-space-threeunits);
+              padding-right: var(--dl-space-space-threeunits);
+            }
+          }
+          @media (max-width: 767px) {
+            .home-hero {
+              padding-left: var(--dl-space-space-twounits);
+              padding-right: var(--dl-space-space-twounits);
+            }
+            .home-text1 {
+              padding-left: var(--dl-space-space-unit);
+              padding-right: var(--dl-space-space-unit);
+            }
+            .home-image {
+              width: 80%;
+            }
+          }
+          @media (max-width: 479px) {
+            .home-hero {
+              padding-top: var(--dl-space-space-twounits);
+              padding-left: var(--dl-space-space-unit);
+              padding-right: var(--dl-space-space-unit);
+              padding-bottom: var(--dl-space-space-twounits);
+            }
+            .home-container1 {
+              margin-bottom: var(--dl-space-space-unit);
+            }
+            .home-btn-group {
+              flex-direction: column;
+            }
+            .home-button1 {
+              margin-top: var(--dl-space-space-unit);
+              margin-left: 0px;
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
